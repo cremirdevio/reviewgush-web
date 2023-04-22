fe44d4064dd1ecb8168ada38eedcae5103df9bfc
cremirdevio <josephajibodu@gmail.com>
2023-04-22 15:57:54 +0100

    chore: landing page updated

--- src/components/auth-layout.tsx
+++ src/components/auth-layout.tsx
@@ -1,6 +1,7 @@
 import { Flex, Heading, Spacer, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
+import Link from './ui/link'
 
 interface AuthLayoutProps {
     children: React.ReactNode
@@ -10,7 +11,9 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <Flex flexDir={"column"} h={"100vh"} bgColor={"orange.100"} justifyContent={"center"} alignItems={"center"}>
         {/* Logo Here */}
-        <Image src={"/logo-primary-hor.svg"} width={"220"} height={"50"} alt="reviewgush logo" />
+        <Link href='/'>
+          <Image src={"/logo-primary-hor.svg"} width={"220"} height={"50"} alt="reviewgush logo" />
+        </Link>
         <Spacer maxH={"32px"} />
         {children}
         {/* Copyright */}
