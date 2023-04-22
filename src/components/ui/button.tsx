4feaa7a38d3c0018be95675b8edc4d5faa811df8
cremirdevio <josephajibodu@gmail.com>
2023-04-22 02:27:24 +0100

    chore: desktop view for login page completed

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,72 @@
+import { ButtonProps, Button as ChakraButton } from "@chakra-ui/react";
+import React from "react";
+
+interface CustomButtonProps extends ButtonProps {
+    variant?: "primary" | "secondary" | "outline";
+}
+
+export default function Button({ children, variant, ...rest }: CustomButtonProps) {
+    let colorScheme;
+    let bg;
+    let hoverBg;
+    let activeBg;
+    let textColor;
+    let hoverTextColor;
+    let activeTextColor;
+    let border;
+
+    switch (variant) {
+        case "primary":
+            colorScheme = "orange";
+            bg = "orange.500";
+            hoverBg = "orange.600";
+            activeBg = "orange.700";
+            textColor = "white";
+            hoverTextColor = "white";
+            activeTextColor = "white";
+            break;
+        case "secondary":
+            colorScheme = "green";
+            bg = "green.400";
+            hoverBg = "green.500";
+            activeBg = "green.600";
+            textColor = "white";
+            hoverTextColor = "white";
+            activeTextColor = "white";
+            break;
+        case "outline":
+            colorScheme = "gray";
+            bg = "transparent";
+            hoverBg = "gray.100";
+            activeBg = "gray.200";
+            textColor = "gray.700";
+            hoverTextColor = "gray.900";
+            activeTextColor = "gray.900";
+            border = "1px solid gray";
+            break;
+        default:
+            colorScheme = "gray";
+            bg = "gray.500";
+            hoverBg = "gray.600";
+            activeBg = "gray.700";
+            textColor = "white";
+            hoverTextColor = "white";
+            activeTextColor = "white";
+            break;
+    }
+
+    return (
+        <ChakraButton
+            colorScheme={colorScheme}
+            bg={bg}
+            borderCol
+            _hover={{ bg: hoverBg, color: hoverTextColor }}
+            _active={{ bg: activeBg, color: activeTextColor }}
+            color={textColor}
+            border={border}
+            {...rest}
+        >
+            {children}
+        </ChakraButton>
+    );
+}
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,15 +1,16 @@
-import { Box, Text, HStack, VStack, Heading, Container, Flex, FormControl, FormLabel, FormHelperText, Spacer, InputGroup, InputLeftAddon, InputLeftElement, Icon, InputRightElement, Button, Link, Checkbox, Input } from '@chakra-ui/react'
+import { Box, Text, HStack, VStack, Heading, Container, Flex, FormControl, FormLabel, FormHelperText, Spacer, InputGroup, InputLeftAddon, InputLeftElement, Icon, InputRightElement, Link, Checkbox, Input } from '@chakra-ui/react'
 import React from 'react'
 import { MdFacebook, MdPassword } from 'react-icons/md'
 import { FaApple, FaEye, FaFacebook, FaGoogle } from 'react-icons/fa'
 import AuthLayout from '@/src/components/auth-layout'
 import FormInput from '@/src/components/ui/input'
 import PasswordInput from '@/src/components/ui/password-input'
+import Button from '@/src/components/ui/button'
 
 export default function Login() {
   return (
     <AuthLayout>
-      <Container w={"md"} bg={"whiteAlpha.900"} borderRadius={'3xl'} height={"fit-content"} padding={"48px"}>
+      <Container shadow={"2xl"} w={"md"} bg={"whiteAlpha.900"} borderRadius={'3xl'} height={"fit-content"} padding={"48px"}>
         <Heading>Hi, Welcome Back!</Heading>
         <Text fontSize={"sm"}>Start 14 day full-featured trial. No credit card required.</Text>
         <Spacer height={"32px"} />
@@ -38,7 +39,7 @@ export default function Login() {
         </Link>
 
         {/* Sign In button */}
-        <Button width={"full"} size={"lg"} bgColor={"orange.500"} color={"white"}>Sign In</Button>
+        <Button width={"full"} size={"lg"} variant={"primary"}>Sign In</Button>
 
         {/* Line: or sign in with */}
         <HStack mt={"16px"} mb={"16px"} justifyContent={"center"} alignItems={"center"}>
