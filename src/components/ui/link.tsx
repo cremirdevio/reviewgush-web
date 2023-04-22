e3ab85ff3393573d2bf27654a1b02b7e0f9a3eac
cremirdevio <josephajibodu@gmail.com>
2023-04-22 02:59:07 +0100

    feat: register page completed

---
+++ src/components/ui/link.tsx
@@ -0,0 +1,15 @@
+import NextLink from "next/link";
+import { Link as ChakraLink, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+
+interface NextChakraLinkProps extends ChakraLinkProps {
+  href?: string;
+  children: React.ReactNode;
+}
+
+export default function Link({ href, children, ...chakraProps }: NextChakraLinkProps) {
+  return (
+    <NextLink href={href ?? "#"} passHref>
+      <ChakraLink {...chakraProps}>{children}</ChakraLink>
+    </NextLink>
+  );
+}
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,7 +1,6 @@
-import { Box, Text, HStack, VStack, Heading, Container, Spacer, Checkbox } from '@chakra-ui/react'
+import { Box, Text, HStack, Heading, Container, Checkbox, Stack, Spacer } from '@chakra-ui/react'
 import React from 'react'
-import { MdFacebook, MdPassword } from 'react-icons/md'
-import { FaApple, FaEye, FaFacebook, FaGoogle } from 'react-icons/fa'
+import { FaApple, FaGoogle } from 'react-icons/fa'
 import AuthLayout from '@/src/components/auth-layout'
 import FormInput from '@/src/components/ui/input'
 import PasswordInput from '@/src/components/ui/password-input'
@@ -13,44 +12,47 @@ export default function Login() {
   return (
     <AuthLayout>
       <Head>
-        <title>Login to ReviewGush</title>
+        <title>Register free ReviewGush account</title>
       </Head>
       <Container shadow={["none", "2xl"]} w={["full", "md"]} bg={"whiteAlpha.900"} borderRadius={["none", "3xl"]} height={"fit-content"} px={["24px", "48px"]} py={["48px"]}>
-        <Heading fontSize={"2xl"}>Hi, Welcome Back!</Heading>
-        {/* <Text fontSize={"sm"} mt={"8px"}>Start 14 day full-featured trial. No credit card required.</Text> */}
+        <Heading fontSize={"2xl"}>Get Started Now</Heading>
+        <Text fontSize={"sm"} mt={"8px"}>Join ReviewGush to easily collect, manage and share your reviews.</Text>
         <Spacer height={"32px"} />
+
+        {/* Firstname and Lastname */}
+        <Stack direction={"row"}>
+          <FormInput
+            type='text' placeholder='First Name' size={"lg"}
+          />
+          <FormInput
+            type='text' placeholder='Last Name' size={"lg"}
+          />
+        </Stack>
+        <Spacer height={"12px"} />
 
         {/* Enter Email/Phone no */}
         <FormInput
           type='email' placeholder='Enter Email' size={"lg"}
-          leftIcon={"@"}
         />
         <Spacer height={"12px"} />
         
         {/* Password */}
-        <PasswordInput />
+        <PasswordInput autoComplete={"false"} />
         <Spacer height={"12px"} />
 
         {/* Keep me signed in */}
         <Checkbox size='lg' colorScheme={"orange"}>
-          Keep me signed in
+          I agree with all service agreement
         </Checkbox>
         <Spacer height={"32px"} />
 
-        {/* Trouble Signing in */}
-        <Link>
-          <Text fontWeight={"bold"} fontSize={"sm"} mt={"16px"} mb={"12px"}>
-            Forgot Password?
-          </Text>
-        </Link>
-
         {/* Sign In button */}
-        <Button width={"full"} size={"lg"} variant={"primary"}>Sign In</Button>
+        <Button width={"full"} size={"lg"} variant={"primary"}>Sign Up</Button>
 
         {/* Line: or sign in with */}
         <HStack mt={"16px"} mb={"16px"} justifyContent={"center"} alignItems={"center"}>
           <Box height={"1px"} width={"20px"} bgColor={"black"} />
-          <Text fontSize={"xs"}>Or Sign in with</Text>
+          <Text fontSize={"xs"}>Or Sign Up with</Text>
           <Box height={"1px"} width={"20px"} bgColor={"black"} />
         </HStack>
 
@@ -61,7 +63,7 @@ export default function Login() {
         </HStack>
 
         {/* Don't have an account? Register Now */}
-        <Text textAlign={"center"} mt={"16px"} fontSize={"sm"}>Don&apos;t have an account? <Link fontWeight={"bold"} href='/auth/register'>Register Now</Link></Text>
+        <Text textAlign={"center"} mt={"16px"} fontSize={"sm"}>Do you have an account? <Link href={"/auth/login"} fontWeight={"bold"}>Login</Link></Text>
       </Container>
     </AuthLayout>
   )
