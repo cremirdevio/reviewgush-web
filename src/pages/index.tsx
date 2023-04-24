31ee1da3b4a17ac236fb40b852b308c463b00ea1
cremirdevio <josephajibodu@gmail.com>
2023-04-24 23:15:07 +0100

    fix: homepage and other next pages formatted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '../styles/Home.module.css'
-import { Box, Center, Divider, Flex, Heading, Text } from '@chakra-ui/react'
-import Link from '../components/ui/link'
+// import { Inter } from "next/font/google";
+import { Box, Center, Divider, Flex, Heading } from "@chakra-ui/react";
+import Link from "../components/ui/link";
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: [`latin`] });
 
 export default function Home() {
   return (
-    <Center flexDir={"column"}>
-      <Heading mt={"6"}>Authentication Pages</Heading>
-      <Box height={"1px"} width={"100px"} bgColor={"orange.500"} my={"24px"} />
-      <Flex gap={"12px"} flexDirection={"column"} alignItems={"center"}>
-        <Link href='/auth/login'>Login Page</Link>
-        <Link href='/auth/register'>Register Page</Link>
-        <Link href='/auth/forgot-password'>Forgot Password Page</Link>
-        <Link href='/auth/reset-password'>Reset Password Page</Link>
-        <Link href='/auth/verify-email'>Verify Email Page</Link>
+    <Center flexDir={`column`}>
+      <Heading mt={`6`}>Authentication Pages</Heading>
+      <Box height={`1px`} width={`100px`} bgColor={`orange.500`} my={`24px`} />
+      <Flex gap={`12px`} flexDirection={`column`} alignItems={`center`}>
+        <Link href="/auth/login">Login Page</Link>
+        <Link href="/auth/register">Register Page</Link>
+        <Link href="/auth/forgot-password">Forgot Password Page</Link>
+        <Link href="/auth/reset-password">Reset Password Page</Link>
+        <Link href="/auth/verify-email">Verify Email Page</Link>
       </Flex>
 
-      <Heading mt={"20"}>Dashboard Pages</Heading>
-      <Box height={"1px"} width={"100px"} bgColor={"orange.500"} my={"24px"} />
-      <Flex gap={"12px"} flexDirection={"column"} alignItems={"center"}>
-        <Link href='/account/dashboard'>Dashboard</Link>
-        <Link href='/account/reviews'>Reviews</Link>
-        <Link href='/account/tags'>Tags</Link>
+      <Heading mt={`20`}>Dashboard Pages</Heading>
+      <Box height={`1px`} width={`100px`} bgColor={`orange.500`} my={`24px`} />
+      <Flex gap={`12px`} flexDirection={`column`} alignItems={`center`}>
+        <Link href="/account/dashboard">Dashboard</Link>
+        <Link href="/account/reviews">Reviews</Link>
+        <Link href="/account/tags">Tags</Link>
         <Divider></Divider>
-        <Link href='/account/form'>Collection Form</Link>
-        <Link href='/account/import-review'>Add Review</Link>
+        <Link href="/account/form">Collection Form</Link>
+        <Link href="/account/import-review">Add Review</Link>
       </Flex>
     </Center>
-  )
+  );
 }
