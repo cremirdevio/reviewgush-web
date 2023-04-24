46c913797f4f9ac6a2331e7e73e8eb4d451fc34c
cremirdevio <josephajibodu@gmail.com>
2023-04-24 22:08:23 +0100

    dep: prettier configurations updated

--- src/pages/auth/forgot-password.tsx
+++ src/pages/auth/forgot-password.tsx
@@ -1,17 +1,34 @@
-import { Box, Text, HStack, VStack, Heading, Container, Spacer, Checkbox, Icon, Flex } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import { MdFacebook, MdPassword } from 'react-icons/md'
-import { FaApple, FaArrowLeft, FaEye, FaFacebook, FaGoogle } from 'react-icons/fa'
-import AuthLayout from '@/src/components/auth-layout'
-import FormInput from '@/src/components/ui/input'
-import PasswordInput from '@/src/components/ui/password-input'
-import Button from '@/src/components/ui/button'
-import Head from 'next/head'
-import Link from '@/src/components/ui/link'
-import Image from 'next/image'
+import {
+  Box,
+  Text,
+  HStack,
+  VStack,
+  Heading,
+  Container,
+  Spacer,
+  Checkbox,
+  Icon,
+  Flex,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { MdFacebook, MdPassword } from "react-icons/md";
+import {
+  FaApple,
+  FaArrowLeft,
+  FaEye,
+  FaFacebook,
+  FaGoogle,
+} from "react-icons/fa";
+import AuthLayout from "@/src/components/auth-layout";
+import FormInput from "@/src/components/ui/input";
+import PasswordInput from "@/src/components/ui/password-input";
+import Button from "@/src/components/ui/button";
+import Head from "next/head";
+import Link from "@/src/components/ui/link";
+import Image from "next/image";
 
 export default function ForgotPassword() {
-  const [resetEmailSent, setResetEmailSent] = useState<Boolean>(false);
+  const [resetEmailSent, setResetEmailSent] = useState<boolean>(false);
 
   return (
     <AuthLayout>
@@ -19,54 +36,106 @@ export default function ForgotPassword() {
         <title>Forgot Password</title>
       </Head>
 
-      {!resetEmailSent ?
-        <Container shadow={["none", "2xl"]} w={["full", "md"]} bg={"whiteAlpha.900"} borderRadius={["none", "3xl"]} height={["full", "fit-content"]} px={["24px", "48px"]} py={["48px"]}>
+      {!resetEmailSent ? (
+        <Container
+          shadow={[`none`, `2xl`]}
+          w={[`full`, `md`]}
+          bg={`whiteAlpha.900`}
+          borderRadius={[`none`, `3xl`]}
+          height={[`full`, `fit-content`]}
+          px={[`24px`, `48px`]}
+          py={[`48px`]}
+        >
           {/* Logo Here */}
-          <Link href='/'>
-            <Image src={"/logo-icon-primary.svg"} width={"36"} height={"36"} alt="reviewgush logo" />
+          <Link href="/">
+            <Image
+              src={`/logo-icon-primary.svg`}
+              width={`36`}
+              height={`36`}
+              alt="reviewgush logo"
+            />
           </Link>
-          <Spacer height={"16px"} />
-          
-          <Heading fontWeight={"normal"} fontSize={["2xl"]}>Forgot Password?</Heading>
-          <Text fontSize={"sm"} mt={"8px"}>No worries, we&apos;ll send you reset instructions.</Text>
-          <Spacer height={"32px"} />
+          <Spacer height={`16px`} />
+
+          <Heading fontWeight={`normal`} fontSize={[`2xl`]}>
+            Forgot Password?
+          </Heading>
+          <Text fontSize={`sm`} mt={`8px`}>
+            No worries, we&apos;ll send you reset instructions.
+          </Text>
+          <Spacer height={`32px`} />
 
           {/* Enter Email */}
-          <FormInput
-            type='email' placeholder='Enter your email' size={"lg"}
-          />
-          <Spacer height={"12px"} />
+          <FormInput type="email" placeholder="Enter your email" size={`lg`} />
+          <Spacer height={`12px`} />
 
           {/* Sign In button */}
-          <Button width={"full"} size={"lg"} variant={"primary"}>Reset Password</Button>
+          <Button width={`full`} size={`lg`} variant={`primary`}>
+            Reset Password
+          </Button>
 
           {/* Back to Login */}
-          <Link mt={"16px"} fontSize={"sm"} fontWeight={"bold"} href='/auth/login' display={"flex"} justifyContent={"center"} alignItems={"center"} gap={"8px"}>
+          <Link
+            mt={`16px`}
+            fontSize={`sm`}
+            fontWeight={`bold`}
+            href="/auth/login"
+            display={`flex`}
+            justifyContent={`center`}
+            alignItems={`center`}
+            gap={`8px`}
+          >
             <Icon as={FaArrowLeft} />
             <Text>Back to Login</Text>
           </Link>
         </Container>
-        :
-        <Container textAlign={"center"} shadow={["none", "2xl"]} w={["full", "md"]} bg={"whiteAlpha.900"} borderRadius={["none", "3xl"]} height={"fit-content"} px={["24px", "48px"]} py={["48px"]}>
-          <Heading fontSize={"2xl"}>Check your email</Heading>
-          <Text fontSize={"sm"} mt={"8px"}>We sent a password reset link to <Text fontWeight={"bold"}>example@reviewgush.com</Text></Text>
-          <Spacer height={"32px"} />
+      ) : (
+        <Container
+          textAlign={`center`}
+          shadow={[`none`, `2xl`]}
+          w={[`full`, `md`]}
+          bg={`whiteAlpha.900`}
+          borderRadius={[`none`, `3xl`]}
+          height={`fit-content`}
+          px={[`24px`, `48px`]}
+          py={[`48px`]}
+        >
+          <Heading fontSize={`2xl`}>Check your email</Heading>
+          <Text fontSize={`sm`} mt={`8px`}>
+            We sent a password reset link to{` `}
+            <Text fontWeight={`bold`}>example@reviewgush.com</Text>
+          </Text>
+          <Spacer height={`32px`} />
 
           {/* Sign In button */}
-          <Button width={"full"} size={"lg"} variant={"primary"}>Open Email App</Button>
+          <Button width={`full`} size={`lg`} variant={`primary`}>
+            Open Email App
+          </Button>
 
-          <Text textAlign={"center"} mt={"16px"} fontSize={"sm"}>
+          <Text textAlign={`center`} mt={`16px`} fontSize={`sm`}>
             Didn&apos;t receive the email?
-            <Link fontWeight={"bold"} href='#'> Click to resend</Link>
+            <Link fontWeight={`bold`} href="/">
+              {` `}
+              Click to resend
+            </Link>
           </Text>
 
           {/* Back to Login */}
-          <Link mt={"16px"} fontSize={"sm"} fontWeight={"bold"} href='/auth/login' display={"flex"} justifyContent={"center"} alignItems={"center"} gap={"8px"}>
+          <Link
+            mt={`16px`}
+            fontSize={`sm`}
+            fontWeight={`bold`}
+            href="/auth/login"
+            display={`flex`}
+            justifyContent={`center`}
+            alignItems={`center`}
+            gap={`8px`}
+          >
             <Icon as={FaArrowLeft} />
             <Text>Back to Login</Text>
           </Link>
         </Container>
-      }
+      )}
     </AuthLayout>
-  )
+  );
 }
