f5fd1c49eb37d4f736acfaac41f08579122bd54c
cremirdevio <josephajibodu@gmail.com>
2023-04-24 23:26:15 +0100

    fix: other missed codes linted

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react'
-import FormInput from './input'
-import { Icon, InputProps, useBoolean } from '@chakra-ui/react'
-import { MdPassword } from 'react-icons/md'
-import { FaEye, FaEyeSlash } from 'react-icons/fa'
+import FormInput from "./input";
+import { Icon, InputProps, useBoolean } from "@chakra-ui/react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function PasswordInput({ ...props }: InputProps) {
-    const [showPassword, {toggle}] = useBoolean();
+  const [showPassword, { toggle }] = useBoolean();
 
   return (
     <FormInput
-        type={showPassword ? "text" : "password"} placeholder='Password' size={"lg"}
-        rightIcon={<Icon onClick={toggle} as={showPassword ? FaEyeSlash : FaEye} />}
-        {...props}
+      type={showPassword ? `text` : `password`}
+      placeholder="Password"
+      size={`lg`}
+      rightIcon={
+        <Icon onClick={toggle} as={showPassword ? FaEyeSlash : FaEye} />
+      }
+      {...props}
     />
-  )
+  );
 }
